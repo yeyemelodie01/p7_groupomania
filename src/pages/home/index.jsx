e25298ea7138c0573b2126a260cd97117a9d77f9
melodieyeye <yeyemelodie@outlook.fr>
2022-07-21 14:02:28 +0200

    added pages modifications

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+function Home() {
+  return (
+    <FontAwesomeIcon icon={ faCirclePlus }/>
+  );
+}
+
+export default Home;
