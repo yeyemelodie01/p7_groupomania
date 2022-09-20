666b04cc9e0b5006949a7d8abb98d151a376f2f6
melodieyeye <yeyemelodie@outlook.fr>
2022-09-20 13:34:22 +0200

    add modifications

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,50 +1,70 @@
 const postModel = require('../models/post.model');
-//const fs = require('fs')
 
 exports.postRequest = async(req, res) => {
     postModel.find()
       .then((post) => req.status(200).json(post))
-      .catch(error => res.status(400).json({ error }));
+      .catch(error => res.status(400).json({error}));
 }
 
 exports.postIdRequest = async(req, res) => {
     postModel.findOne({ _id: req.params.id })
       .then((post) => req.status(200).json(post))
-      .catch(error => res.status(400).json({ error }));
+      .catch(error => res.status(400).json({error}));
 }
 
 exports.postAddRequest = async(req, res) => {
   const {userId, postType, post} = req.body;
-  const dataPost = await postModel.find({title: post.title});
+  let title = "";
+  if ('media' === postType) {
+      title = req.body.title;
+  }
+
+  if ('text' === postType) {
+      title = post.title;
+  }
+
+  const dataPost = await postModel.find({title: title});
   if(dataPost.length > 0) {
-    if ('media' === postType) {
-      const dataUpdate = req.file ? {
-        ...req.body,
-        imageUrl: `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}` // url pour l'image req.protocol(http), req.get('host) pour l'hôte de serveur ici localhost:3000, uploads(dossier qui contiendra l'image), req.file.filename pour le nom du fichier
-    } : { ...req.body };
-      console.log(dataUpdate);
-    }
     res.status(200).json({message: "post déja ajouter"});
     return;
   }
+
   try {
-    const newPost = new postModel({
-      "userId": userId,
-      "title": post.title,
-      "text": post.text,
-      "like": 0,
-      "dislike": 0,
-      "usersLiked": [],
-      "usersDisLiked": [],
-    });
-    newPost.save()
-      .then(() => res.status(201).json({message: "post ajouter"}))
-      .catch(error =>  res.status(400).json({error}));
+    if ('media' === postType) {
+      const newPost = new postModel({
+        "userId": userId,
+        "title": title,
+        "media": `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`,
+        "like": 0,
+        "dislike": 0,
+        "usersLiked": [],
+        "usersDisLiked": [],
+      });
+      return savePosts(newPost, res);
+    }
+
+    if ('text' === postType) {
+      const newPost = new postModel({
+        "userId": userId,
+        "title": title,
+        "text": post.text,
+        "like": 0,
+        "dislike": 0,
+        "usersLiked": [],
+        "usersDisLiked": [],
+      });
+      return savePosts(newPost, res);
+    }
   } catch (error) {
     res.status(400).json({error})
   }
 }
 
+function savePosts(post, res) {
+    return post.save()
+        .then(() => res.status(201).json({message: "post ajouter"}))
+        .catch(error =>  res.status(400).json({error}));
+}
 //exports.postUpdateRequest = async(req) => {
 //    const dataUpdate = req.file ? {
 //                 ...req.body,//
