53b1cf12642df71bf0b2943967ea5705b648e3ba
melodieyeye <yeyemelodie@outlook.fr>
2022-11-08 12:27:33 +0100

    add modifications

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './pages/home';
 import Header from "./components/header";
 import GlobalStyle from './utils/styles/GlobalStyle'
 import Detailposts from './pages/detailposts'
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
-ReactDOM.render(
-  <React.StrictMode>
-      <Router>
-          <GlobalStyle />
-          <Header />
-          <Route exact path="/">
-              <Home />
-          </Route>
-        <Route path="/detail">
-              <Detailposts />
-        </Route>
-      </Router>
-  </React.StrictMode>,
-    document.getElementById('root')
+root.render(
+  <StrictMode>
+    <Router>
+      <GlobalStyle />
+      <Header />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/detail">
+        <Detailposts />
+      </Route>
+    </Router>
+  </StrictMode>,
 );
